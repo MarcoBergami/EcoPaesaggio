197c522cac1cc8fc2c09cf76e95105e7bfb80070
MarcoBergami <62288864+MarcoBergami@users.noreply.github.com>
2020-04-06 12:52:14 +0200

    Update R_code_spatial.r

--- R_code_spatial.r
+++ R_code_spatial.r
@@ -7,17 +7,19 @@ attach(meuse)     #alleghiamo il dataframe
 plot(cadmium,lead,col="red",pch=19,cex=2)
 plot(copper,zinc,col="green",pch=17,cex=2)  #plot di rame e zinco usando come simbolo il triangolo e colore verde
 plot(copper,zinc,col="green",pch=17,cex=2,xlab="rame", ylab="zinco")
+
 # funzione multiframe o multipanel, necessaria per mostrare più grafici insieme, si chiama "par", in questo caso composto da 1 riga e 2 colonne.
 par(mfrow=c(1,2))
 plot(cadmium,lead,col="red",pch=19,cex=2)
 plot(copper,zinc,col="green",pch=17,cex=2)
 # ovviamente per utilizzare un multipanel con 2 righe e 1 colonna basta cambiare i numeri dentro la parentsi di c.
+
 install.packages("GGally")
 library(GGally)
-#richiamiamo la funzione pairs in GGally, ma facendo un sottoinsieme delle analisi che vengono svolte
+#richiamiamo la funzione pairs in GGally specificando un sottoinsieme delle variabili
 ggpairs(meuse[,3:6])
 # sull'asse diagonale sono presenti le singole variabili e la distribuzione di frequenza dei dati, nel pannello superiore ci sono i valori del coeff. di correlazione.
-# la correlazione risulta essere positiva in tutti i casi e anche abbastanza alta, vicino ad 1 tranne nel caso del cadmio. 
+
 # per svolgere delle analisi spaziali occorre per prima cosa far capire ad R che il dataframe contiene anche dei valori di coordinate, x e y. per farlo usiamo una funzione "coordinates" contenuta nel pacchetto sp.
 coordinates(meuse)=~x+y # la tilde è necessaria per raggruppare più colonne del dataset
 plot(meuse)
@@ -25,6 +27,7 @@ spplot(meuse,"zinc") #funzione spplot per plottare i dati distribuiti nello spaz
 # funzione bubble del pacchetto sp. Permette di plottare allo stesso modo di prima ma usando dei pallini più o meno grandi a seconda delle concentrazioni
 bubble(meuse,"zinc")
 bubble(meuse,"copper",col="red")
+
 # inventiamo 2 oggetti attraverso la definizione di stringhe
 foram <- c(10, 20, 35, 55, 67, 80)
 carbon <- c(5, 15, 30, 70, 85, 99)
