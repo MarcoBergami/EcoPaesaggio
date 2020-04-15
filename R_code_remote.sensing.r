8d1e60db5bf5393b78625316e1902d642a0b49f5
MarcoBergami <62288864+MarcoBergami@users.noreply.github.com>
2020-04-15 13:44:43 +0200

    Update R_code_remote.sensing.r

--- R_code_remote.sensing.r
+++ R_code_remote.sensing.r
@@ -53,3 +53,74 @@ dev.off()
 
 plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="Lin") # visualizziamo la vegetazione verde e il suolo viola 
 plotRGB(p224r63_2011, r=3, g=2, b=4, stretch="Lin") # visualizziamo la vegetazione blu e il suolo giallo
+
+p224r63_1988 <- brick("p224r63_1988_masked.grd") # svolgiamo il medesimo esercizio sull'immagine del 1988
+par(mfrow=c(2,2)) 
+clb <- colorRampPalette(c('dark blue','blue','light blue'))(100)
+plot(p224r63_1988$B1_sre,col=clb)
+clg <- colorRampPalette(c('dark green','green','light green'))(100)
+plot(p224r63_1988$B2_sre,col=clg)
+clr <- colorRampPalette(c('dark red','red','pink'))(100)
+plot(p224r63_1988$B3_sre,col=clr)
+clnir <- colorRampPalette(c('red','orange','yellow'))(100)
+plot(p224r63_1988$B4_sre,col=clnir)
+# le color palette delle varie bande si potrebbero anche non riscrivere in quanto già salvate nella sessione precedente
+dev.off()
+
+plotRGB(p224r63_1988,r=3,g=2,b=1, stretch="Lin")
+plotRGB(p224r63_1988,r=4,g=3,b=2, stretch="Lin")
+
+#plot delle immagini dei due anni messe a confronto
+par(mfrow=c(2,1))
+plotRGB(p224r63_1988,r=4,g=3,b=2, stretch="Lin")
+plotRGB(p224r63_2011,r=4,g=3,b=2, stretch="Lin")
+dev.off()
+
+#spectral indices - DVI
+dvi1988 <- p224r63_1988$B4_sre - p224r63_1988$B3_sre # creiamo la nuova immagine data dalla sottrazione, pixel per pixel, tra la banda dell'infrarosso e quella del rosso
+dvi2011 <- p224r63_2011$B4_sre - p224r63_2011$B3_sre # lo stesso per l'anno 2011
+
+# multitemporal analysis
+difdvi <- dvi2011 - dvi1988 # calcoliamo la differenza nell'indice per i due anni
+cldifdvi <- colorRampPalette(c('red','white','blue'))(100)
+plot(difdvi, col=cldifdvi)
+
+
+par(mfrow=c(3,1)) # creiamo il multipanel con l'infrarosso dei due anni e la differenza nell'indice DVI
+plotRGB(p224r63_1988,r=4,g=3,b=2, stretch="Lin")
+plotRGB(p224r63_2011,r=4,g=3,b=2, stretch="Lin")
+plot(difdvi, col=cldifdvi)
+dev.off()
+
+# Changing the grain (Resolution)
+p224r63_2011 # possiamo visualizzare na tura del file e la sua risoluzione
+p224r63_2011lr # lo stesso per la nuova risoluzione
+
+p224r63_2011lr <- aggregate(p224r63_2011, fact=10) # lr = low resolution con ingrandimento del pixel di 10 volte (media dei pixel originali contenuti nel nuovo pixel più grande)
+par(mfrow=c(2,1))
+plotRGB(p224r63_2011,r=4,g=3,b=2, stretch="Lin")
+plotRGB(p224r63_2011lr,r=4,g=3,b=2, stretch="Lin")
+
+p224r63_2011lr50 <- aggregate(p224r63_2011, fact=50) # ingrandimento di 50 volte
+
+par(mfrow=c(3,1)) # mettiamo a confronto le varie risoluzioni sull'immagine del 2011
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")
+plotRGB(p224r63_2011lr, r=4, g=3, b=2, stretch="Lin")
+plotRGB(p224r63_2011lr50, r=4, g=3, b=2, stretch="Lin")
+
+dvi2011lr50 <- p224r63_2011lr50$B4_sre - p224r63_2011lr50$B3_sre # creiamo l'indice per il 2011 ma a bassa risoluzione
+plot(dvi2011lr50)
+
+p224r63_1988lr50 <- aggregate(p224r63_1988, fact=50) 
+dvi1988lr50 <- p224r63_1988lr50$B4_sre - p224r63_1988lr50$B3_sre
+difdvilr50 <- dvi2011lr50 - dvi1988lr50
+cldifdvi <- colorRampPalette(c('red','white','blue'))(100)
+plot(difdvilr50, col=cldifdvi)
+dev.off()
+
+par(mfrow=c(2,1)) # confrontiamo la variazione dell'indice nelle due risoluzioni
+plot(difdvi, col=cldifdvi)
+plot(difdvilr50, col=cldifdvi)
+
+
+
